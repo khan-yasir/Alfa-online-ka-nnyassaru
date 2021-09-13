390f86e4e6eb28e6e92946f3cab21f54224fd35c
kan yassaru <89886786+khan-yasir@users.noreply.github.com>
2021-09-13 11:26:48 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'ろく',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'はね',
           // q2: 'えええ',
           // q3: 'おおお'
         },
